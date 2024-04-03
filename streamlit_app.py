c93c18bc56afdb9b1fb6ae5c3e37ed365dc8b1f3
Rifat Monzur <rifatmonzur@gmail.com>
2024-04-03 20:58:28 +0100

    Open AI base implementation added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import openai
+
+
+
+def setup_openai():
+    openai.api_key = st.secrets['OPENAI_API_KEY']
+    client = openai.OpenAI()
+    return client
+
+def get_completion(prompt, model="gpt-3.5-turbo-0613"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return response.choices[0].message.content
 
 def create_ui():
     with st.form("my_form"):
@@ -12,5 +29,14 @@ def create_ui():
         # Every form must have a submit button.
         submitted = st.form_submit_button('Submit')
 
+        if submitted:
+            response = get_completion("who is the best footballer?")
+            print(response)
 
+
+openai.api_key = st.secrets['OPENAI_API_KEY']
+client = openai.OpenAI()
 create_ui()
+print("running")
+#response = get_completion("who is the best footballer?")
+#print(response)
