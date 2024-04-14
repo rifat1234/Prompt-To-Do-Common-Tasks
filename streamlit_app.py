5da9f82ee5c38e8fc9ab27312666f08e16854342
Rifat Monzur <rifatmonzur@gmail.com>
2024-04-14 18:59:55 +0100

    Summary prompt added

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ def setup_openai():
     client = openai.OpenAI()
     return client
 
+
 def get_completion(prompt, model="gpt-3.5-turbo-0613"):
     messages = [{"role": "user", "content": prompt}]
     response = client.chat.completions.create(
@@ -18,6 +19,7 @@ def get_completion(prompt, model="gpt-3.5-turbo-0613"):
     )
     return response.choices[0].message.content
 
+
 @st.experimental_fragment
 def add_copy(text):
     copy_button = st.button("Copy to clipboard")
@@ -25,20 +27,19 @@ def add_copy(text):
         pyperclip.copy(text)
         st.toast("Copied to clipboard")
 
+
 def create_ui():
     task_summarise = 'Summarise'
     task_proofread = 'Proofread'
     option = st.selectbox(
-        'Choose your preferred Task',
-        (task_summarise, task_proofread))
+        'Choose Task',
+        (task_proofread, task_summarise))
     txt = st.text_area(f'Write your text', height=200,
                        value="", max_chars=10000)
 
     if option == task_summarise:
-        word_limit = st.number_input("Summarise word limit", value=30, min_value=10, max_value=300)
-
-
-
+        word_limit = st.number_input("Summarise word limit (GPT is not always accurate)", value=60, min_value=5,
+                                     max_value=600)
 
     submitted = st.button('Submit')
 
@@ -61,14 +62,13 @@ def create_ui():
                 diff = Redlines(txt, response)
                 st.markdown(diff.output_markdown, unsafe_allow_html=True)
 
-
-        #if option == task_summarise:
-        # prompt = f"Summarise the text inside "
-        # print(prompt)
-        # response = get_completion("who is the best footballer?")
-        # print(response)
+        if option == task_summarise:
+            prompt = f"Summarise the text inside delimited by triple backticks within {word_limit} words: '''{txt}''' "
+            response = get_completion(prompt)
+            word_count = len(response.split())
+            st.text_area(f'Summary (word count: {word_count})', height=200, value=response, disabled=True)
+            add_copy(response)
 
 
 client = setup_openai()
 create_ui()
-
